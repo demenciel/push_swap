70d0e72de42566c72599b7e46c886511f5cfde65
Alexandre Couture <acouture@c1r5p1.42quebec.com>
2023-03-30 14:34:31 -0400

    gay

--- src/sorting/sorting_big.c
+++ src/sorting/sorting_big.c
@@ -6,7 +6,7 @@
 /*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 08:55:24 by acouture          #+#    #+#             */
-/*   Updated: 2023/03/29 17:00:19 by acouture         ###   ########.fr       */
+/*   Updated: 2023/03/30 14:33:03 by acouture         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,20 +45,21 @@ void    sorting_big(t_data *data)
     int len;
     int y;
 
-    median = avg_of_pile(data->pile_a, data->size_pile_a) / 10;
+    median = avg_of_pile(data->pile_a, data->size_pile_a) / 6;
     median_add = median;
     len = data->size_start_pile;
-    while (median)
+    while (median < (len - (median_add * 2)))
     {
         y = 0;
-        while (y < median)
+        while ((y <= median))
         {
-            if ((*data->pile_a)->data >= median)
+            if ((*data->pile_a)->data > median)
                 first_to_last(data->pile_a, 'a', true);
             else
                 push_b(data);
             y++;
         }
+        ft_printf("%d\n", median);
         median += median_add;
     }
     sort_a_500(data);
